94af6ce0e8808b79648b6f4f6786da5aeb6ca1b7
marcosvinicius69073-rgb <marcosvinicius69073@gmail.com>
2026-08-28 13:17:31 -0300

    scipts_atv

---
+++ scripts/app3.js
@@ -0,0 +1,15 @@
+let nasc = 1991;
+let nome = "Pedrina";
+let viva = true;
+let altura = 1.64;
+let sexo;
+let peso = null;
+let imc = "peso"*10;
+
+alert(`ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`nome - tipo: ${typeof(nome)}`)
+alert(`viva - tipo: ${typeof(viva)}`)
+alert(`Altura - tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso  - tipo: ${typeof(peso)}`)
+alert(`IMC - tipo: ${typeof(imc)}`)
